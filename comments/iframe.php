6af8db2148fe66c10c1a1f65a4bbafa482c5ec21
Granzottomccarthy <75643573+Granzottomccarthy@users.noreply.github.com>
2020-12-14 07:18:19 -0500

    Add files via upload

---
+++ comments/iframe.php
@@ -0,0 +1,45 @@
+<?php
+session_name('commentics-iframe-session');
+
+// Default session parameters
+$session_parameters = [
+    'cookie_httponly'  => 1,
+    'use_only_cookies' => 1,
+    'use_trans_sid'    => 0,
+    'gc_maxlifetime'   => 1440,
+    'cookie_lifetime'  => 0,
+    'cookie_path'      => '/',
+    'cookie_secure'    => 1
+];
+
+/*
+ * Set session cookie as SameSite = None to allow
+ * cross-domain for the iFrame integration method
+ */
+if (version_compare(PHP_VERSION, '7.3.0', '>=')) {
+    $session_parameters['cookie_samesite'] = 'None';
+} else {
+    $session_parameters['cookie_path'] = '/; SameSite=None';
+}
+
+session_start($session_parameters);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Commentics</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="frontend/view/default/javascript/iframe.js"></script>
+</head>
+<body class="cmtx_iframe_body" style="margin:0">
+<?php
+if (isset($_GET['identifier']) && isset($_GET['reference']) && isset($_GET['url'])) {
+    $cmtx_identifier = $_GET['identifier'];
+    $cmtx_reference  = $_GET['reference'];
+    $cmtx_page_url   = $_GET['url'];
+    require('frontend/index.php');
+}
+?>
+</body>
+</html>
