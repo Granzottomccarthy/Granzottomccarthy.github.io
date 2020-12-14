c8ccf1b6e739a7683f8b8f7a102501e5f2050dc6
Granzottomccarthy <75643573+Granzottomccarthy@users.noreply.github.com>
2020-12-14 07:20:36 -0500

    Add files via upload

---
+++ comments/install/controller/main/upgrade_2.php
@@ -0,0 +1,93 @@
+<?php
+namespace Commentics;
+
+class MainUpgrade2Controller extends Controller
+{
+    public function index()
+    {
+        if (!$this->db->isConnected() || !$this->db->isInstalled()) {
+            $this->response->redirect('start');
+        }
+
+        $this->loadLanguage('main/upgrade_2');
+
+        if ($this->request->server['REQUEST_METHOD'] == 'POST') {
+            $this->loadModel('main/upgrade_2');
+
+            $valid_upgrade = true;
+
+            $installed_version = $this->model_main_upgrade_2->getInstalledVersion();
+
+            switch ($installed_version) {
+                case '4.0':
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '3.4':
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '3.3':
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '3.2':
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '3.1':
+                    $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '3.0':
+                    $this->model_main_upgrade_2->upgrade('3.0 -> 3.1');
+                    $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');
+                    break;
+                case '2.5':
+                    $this->model_main_upgrade_2->upgrade('2.5 -> 3.0');
+                    $this->model_main_upgrade_2->upgrade('3.0 -> 3.1');
+                    $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
+                    $this->model_main_upgrade_2->upgrade('4.0 -> 4.1');                    
+                    break;
+                default:
+                    $valid_upgrade = false;
+            }
+
+            if ($valid_upgrade) {
+                $this->model_main_upgrade_2->setVersion();
+
+                if ($this->db->getQueryError()) {
+                    $this->data['error'] = $this->db->getQueryError();
+                } else {
+                    remove_directory(CMTX_DIR_CACHE . 'database/', false);
+                    remove_directory(CMTX_DIR_CACHE . 'modification/', false);
+                    remove_directory(CMTX_DIR_CACHE . 'template/', false);
+
+                    $this->data['lang_info_backend'] = sprintf($this->data['lang_info_backend'], '../' . $this->model_main_upgrade_2->getBackendFolder());
+                }
+            } else {
+                $this->response->redirect('start');
+            }
+        } else {
+            $this->response->redirect('start');
+        }
+
+        $this->data['page'] = '6';
+
+        $this->components = array('common/header', 'common/footer');
+
+        $this->loadView('main/upgrade_2');
+    }
+}
